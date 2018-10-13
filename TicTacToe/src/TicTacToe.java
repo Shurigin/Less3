dd0ce03f3ecc465d029b59c07fb91eeef01c8893
Shurigin <das-999@mail.ru>
2018-10-13 16:31:27 +0300

    first commit

---
+++ TicTacToe/src/TicTacToe.java
@@ -0,0 +1,14 @@
+public class TicTacToe {
+    //Константы
+    private static char[][] map;
+    private static int SIPE = 3;
+
+    private static final char DOT_EMPTY = "●";
+    private static final char DOT_X = "X";
+    private static final char DOT_0 = "0";
+
+
+    public static void main(String[] args) {
+
+    }
+}
